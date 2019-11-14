cacbfc560124872d33fc5fa1266bbaa02304cd7c
Arnold Ruiz <arnoldruiz115@csu.fullerton.edu>
2019-11-13 22:35:17 -0800

    Put results in text boxes so you can copy/paste

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Window(Frame):
         self.hash_type = IntVar()
         self.input_message = StringVar()
 
-        self.hashed_message = Label(self.master)
+        self.hashed_message = Entry(self.master, width=50)
 
         self.init_window()
 
@@ -22,18 +22,18 @@ class Window(Frame):
         Label(self.master, text="Encrypted Message: ").grid(row=0, sticky=tkinter.W)
 
         # hash_input is the text box that the user can type into, text goes to input_hash
-        hash_input = Entry(self.master, textvariable=self.input_hash)
+        hash_input = Entry(self.master, textvariable=self.input_hash, width=50)
         hash_input.grid(row=0, column=1, sticky=tkinter.W)
 
         result = Label(self.master, text="Decrypted: ")
-        result.grid(row=2, sticky=tkinter.W)
+        result.grid(row=1, sticky=tkinter.W)
 
         # Decrypt button calls the decrypt function
         decrypt_button = Button(self.master, text="Decrypt", command=self.decrypt)
         decrypt_button.grid(row=3, column=0, sticky=tkinter.W)
 
         Label(self.master, text="Enter Message: ").grid(row=4, column=0, sticky=tkinter.W)
-        text_input = Entry(self.master, textvariable=self.input_message)
+        text_input = Entry(self.master, textvariable=self.input_message, width=50)
         text_input.grid(row=4, column=1, sticky=tkinter.W)
 
         sha_radio = Radiobutton(self.master, text='SHA1', variable =self.hash_type, value=0)
@@ -51,16 +51,18 @@ class Window(Frame):
         # TODO: Take the text string and pass it to decryption algorithm
         text = self.input_hash.get()
 
-        # TODO: Print the decrypted message
-        Label(self.master, text="                                        ").grid(row=1, column=1, sticky=tkinter.W)
-        Label(self.master, text=text).grid(row=1, column=1, sticky=tkinter.W)
-        print(text)
+        # TODO: Get the decrypted message and store it in 'text'
+
+        text_object = StringVar()
+        text_object.set(text)
+        Label(self.master, text=" " * 80).grid(row=1, column=1, sticky=tkinter.W)
+        Entry(self.master, textvariable=text_object, width=50).grid(row=1, column=1, sticky=tkinter.W)
 
     def hash(self):
         choice = self.hash_type.get()
         switcher = {
-            0:hashlib.sha1,
-            1:hashlib.md5,
+            0: hashlib.sha1,
+            1: hashlib.md5,
         }
         hash_func = switcher.get(choice, lambda: 'default')
         
@@ -70,12 +72,14 @@ class Window(Frame):
             hashed_text = bytes(text, 'utf-8')
             hash_object = hash_func(hashed_text)
             digest = hash_object.hexdigest()
-            self.hashed_message.config(text=digest)
-            print(digest)
+            digest_object = StringVar()
+            digest_object.set(digest)
+            self.hashed_message.config(textvariable=digest_object, width=50)
+
 
 root = Tk()
 # size of window
-root.geometry("400x200")
+root.geometry("500x300")
 
 
 app = Window(root)
